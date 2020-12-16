3d50ff7b6586ae30403644e0d1b150aac06059b2
Chris Bishop <chris@bishop.tech>
2020-12-16 12:17:14 -0500

    Completed Lesson 5

--- starter-files/gatsby/src/pages/index.js
+++ starter-files/gatsby/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
     <div>
+      <Nav />
       <p>Hey i'm the home page! hello</p>
     </div>
   );
