8835c7e5e3b55a0dba45d32ce66b8dd910c448f5
Chris Bishop <chris@bishop.tech>
2020-12-18 14:34:18 -0500

    Completed Lesson 17

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,8 +1,25 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: 'Slicks Slices',
     siteUrl: 'https://gatsby.pizza',
     description: 'Super cool Pizza place!',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // Name of the plugin
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'v7vq0cm0',
+        dataset: 'production',
+        watchMode: true,
+        // any token that starts with gatsby_ will be loaded into the browser by gatsby which is why we need dotenv package and this token name
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
