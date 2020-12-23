1306787535037b851f2d9710dff0cc350405a8eb
Chris Bishop <chris@bishop.tech>
2020-12-23 09:53:09 -0500

    completed lesson 43

--- starter-files/gatsby/src/pages/order.js
+++ starter-files/gatsby/src/pages/order.js
@@ -56,6 +56,7 @@ export default function OrderPage({ data }) {
             value={values.email}
             onChange={updateValue}
           />
+          {/* for honey pot  */}
           <input
             type="mapleSyrup"
             name="mapleSyrup"
